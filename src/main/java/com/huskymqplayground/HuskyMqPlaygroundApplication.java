d2beb28cf7b6b781459643951baba55a2333c315
WL <yongchunl@student.unimelb.edu.au>
2025-12-28 14:23:37 +1100

    init

---
+++ src/main/java/com/huskymqplayground/HuskyMqPlaygroundApplication.java
@@ -0,0 +1,13 @@
+package com.huskymqplayground;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HuskyMqPlaygroundApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HuskyMqPlaygroundApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/huskymqplayground/HuskyMqPlaygroundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.huskymqplayground;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HuskyMqPlaygroundApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
